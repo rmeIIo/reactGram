1b11e4b93a4962dc28d9e1072779556d92ef447d
rmeiio <j.rogercorreamello@gmail.com>
2024-09-09 07:52:40 -0300

    Conexão com o banco

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,3 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
 
-// Connection
+const conn = async () => {
+  try {
+    const dbConn = await mongoose.connect(
+      `mongodb+srv://${dbUser}:${dbPassword}@cluster0.pmop9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+    );
+
+    console.log("Conectou ao banco!");
+
+    return dbConn;
+  } catch (error) {
+    console.log(error)
+  }
+};
+
+conn();
+
+module.exports = conn;
