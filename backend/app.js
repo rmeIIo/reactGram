eb1e3bb955fa36f196fcbad1d0f24a143038d105
rmeiio <j.rogercorreamello@gmail.com>
2024-09-09 07:30:45 -0300

    Rota de teste da API

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// routes
+const router = require("./routes/Router.js");
+app.use(router);
+
 app.listen(port, () => {
   console.log(`App rodando na porta ${port}`);
 });
