77ec4685efa1e5503d5dd030d40b1224a0e7b341
rmeiio <j.rogercorreamello@gmail.com>
2024-09-11 09:15:51 -0300

    Registro do usuário

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
 //const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
@@ -14,9 +15,35 @@ const generateToken = (id) => {
 
 // Register user and sign in
 const register = async (req, res) => {
-  res.send("Registro");
+  const { name, email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user) {
+    res.status(422).json({ errors: "Por favor, utilize outro e-mail." });
+    return;
+  }
+
+  const salt = await bcrypt.genSalt();
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const newUser = await User.create({
+    name,
+    email,
+    password: passwordHash,
+  });
+
+  if(!newUser) {
+    res.status(422).json({errors: ["Houve um erro, por favor tente mais tarde."]})
+    return
+  }
+
+  res.status(201).json({
+    _id: newUser._id,
+    token: generateToken(newUser._id),
+  })
 };
 
 module.exports = {
   register,
-}
+};
