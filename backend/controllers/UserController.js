c5e1ff73064d9ac407c953ba41dbf226bd8d066b
Roger Correa <136924837+rmeIIo@users.noreply.github.com>
2025-02-04 20:56:44 -0300

    feat: user controller

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -33,41 +33,50 @@ const register = async (req, res) => {
     password: passwordHash,
   });
 
-  if(!newUser) {
-    res.status(422).json({errors: ["Houve um erro, por favor tente mais tarde."]})
-    return
+  if (!newUser) {
+    res
+      .status(422)
+      .json({ errors: ["Houve um erro, por favor tente mais tarde."] });
+    return;
   }
 
   res.status(201).json({
     _id: newUser._id,
     token: generateToken(newUser._id),
-  })
+  });
 };
 
 // Sign user in
 const login = async (req, res) => {
-  const {email, password} = req.body;
+  const { email, password } = req.body;
 
-  const user = await User.findOne({email})
+  const user = await User.findOne({ email });
 
-  if(!user) {
-    res.status(404).json({errors: ["Usuário não encontrado."]})
-    return
+  if (!user) {
+    res.status(404).json({ errors: ["Usuário não encontrado."] });
+    return;
   }
 
-  if(!(await bcrypt.compare(password, user.password))) {
-    res.status(422).json({errors: ["Senha inválida."]})
-    return
+  if (!(await bcrypt.compare(password, user.password))) {
+    res.status(422).json({ errors: ["Senha inválida."] });
+    return;
   }
 
   res.status(201).json({
     _id: user._id,
     profileImage: user.profileImage,
     token: generateToken(user._id),
-  })
-}
+  });
+};
+
+const getCurrentUser = async (req, res) => {
+  const user = req.user;
+
+  res.status(200).json(user);
+};
 
 module.exports = {
   register,
-  login
+  login,
+  getCurrentUser,
 };
