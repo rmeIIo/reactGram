17957c6eaa1d439174433285fc6731d656819525
Roger Correa <136924837+rmeIIo@users.noreply.github.com>
2025-03-17 19:25:35 -0300

    inserindo foto

---
+++ backend/controllers/PhotoController.js
@@ -0,0 +1,31 @@
+const Photo = require("../models/Photo");
+const User = require("../models/User");
+
+const mongoose = require("mongoose");
+
+// Insert a photo, with an user related to it
+const insertPhoto = async (req, res) => {
+  const { title } = req.body;
+  const image = req.file.filename;
+
+  const reqUser = req.user;
+
+  const user = await User.findById(reqUser._id);
+
+  const newPhoto = await Photo.create({
+    image,
+    title,
+    userId: user._id,
+    userName: user.name,
+  });
+
+  if (!newPhoto) {
+    return res.status(422).json({ error: "Could not insert photo" });
+  }
+
+  res.status(201).json(newPhoto);
+};
+
+module.exports = {
+  insertPhoto,
+};
--- backend/routes/PhotoRoutes.js
+++ backend/routes/PhotoRoutes.js
@@ -1,13 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Controller
+const { insertPhoto } = require("../controllers/PhotoController");
 
 // Middlewares
-const {photoInsertValidation} = require('../middlewares/photoValidation');
-const authGuard = require('../middlewares/authGuard');
-const validate = require('../middlewares/handleValidation');
+const { photoInsertValidation } = require("../middlewares/photoValidation");
+const authGuard = require("../middlewares/authGuard");
+const validate = require("../middlewares/handleValidation");
+const imageUpload = require("../middlewares/imageUpload");
 
 // Routes
+router.post(
+  "/",
+  authGuard,
+  imageUpload.single("image"),
+  photoInsertValidation(),
+  validate,
+  insertPhoto
+);
 
 module.exports = router;
