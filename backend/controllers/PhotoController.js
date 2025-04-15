0e9eb5a3d1b25a6a63a7ac476533c39766ab7d42
Roger Correa <136924837+rmeIIo@users.noreply.github.com>
2025-04-15 20:25:42 -0300

    funcionalidade de busca de fotos

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -171,6 +171,14 @@ const commentPhoto = async (req, res) => {
   });
 };
 
+const searchPhotos = async (req, res) => {
+  const { q } = req.query;
+
+  const photos = await Photo.find({ title: new RegExp(q, "i") }).exec();
+
+  return res.status(200).json(photos);
+};
+
 module.exports = {
   insertPhoto,
   deletePhoto,
@@ -180,4 +188,5 @@ module.exports = {
   updatePhoto,
   likePhoto,
   commentPhoto,
+  searchPhotos,
 };
--- backend/routes/PhotoRoutes.js
+++ backend/routes/PhotoRoutes.js
@@ -11,6 +11,7 @@ const {
   updatePhoto,
   likePhoto,
   commentPhoto,
+  searchPhotos,
 } = require("../controllers/PhotoController");
 
 // Middlewares
@@ -38,6 +39,8 @@ router.delete("/:id", authGuard, deletePhoto);
 router.get("/", authGuard, getAllPhotos);
 
 router.get("/user/:id", authGuard, getUserPhotos);
+
+router.get("/search", authGuard, searchPhotos);
 
 router.get("/:id", authGuard, getPhotoById);
 
