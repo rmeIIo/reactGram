ca28b119d70ebc7e935ae23660f504bfa2c8fd8e
Roger Correa <136924837+rmeIIo@users.noreply.github.com>
2025-04-22 19:25:23 -0300

    configurando react router

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+
+// Routes
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+// Pages
+import Home from "./pages/Home/Home";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
